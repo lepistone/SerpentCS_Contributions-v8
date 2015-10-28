54ae0fb956134b158750de6033ad03da2a0eb1eb
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2015-10-28 10:15:51 +0100

    lint whitespace problems

--- label/report/dynamic_label.py
+++ label/report/dynamic_label.py
@@ -24,7 +24,7 @@ from openerp.report import report_sxw
 from openerp.osv.orm import browse_record
 
 class report_dynamic_label(report_sxw.rml_parse):
-            
+
     def get_data(self,row,columns,ids,model,number_of_copy):
         active_model_obj = self.pool.get(model)
         label_print_obj = self.pool.get('label.print')
@@ -41,27 +41,27 @@ class report_dynamic_label(report_sxw.rml_parse):
                     if field.python_expression and field.python_field:
                         string = field.python_field.split('.')[-1]
                         value = eval(field.python_field, {'obj': datas})
-                        
+
                     elif field.field_id.name:
                         string = field.field_id.field_description
                         value = getattr(datas, field.field_id.name)
-                        
+
                     if not value:
                         continue
-                    
+
                     if isinstance(value, browse_record):
                         model_obj = self.pool.get(value._name)
                         value = eval("obj." + model_obj._rec_name, {'obj': value})
 
-                         
+
                     if not value:
                         value = ''
-                        
+
                     if field.nolabel:
                         string='';
                     else :
                         string+=' :- '
-                    
+
                     if field.type == 'image' or field.type == 'barcode':
                         string = '';
                         if field.position != 'bottom':
@@ -77,13 +77,13 @@ class report_dynamic_label(report_sxw.rml_parse):
                         vals.append(vals_dict)
                 if bot_dict != {}:
                     vals.append(bot_dict)
-                if vals[0]['value'] not in value_vals:    
+                if vals[0]['value'] not in value_vals:
                     value_vals.append(vals[0]['value'])
                 result.append(vals)
                 temp = vals
 
-        
-        newlist_len = 0        
+
+        newlist_len = 0
         new_list = []
         result1 = []
         list_newdata=[]
@@ -93,12 +93,12 @@ class report_dynamic_label(report_sxw.rml_parse):
                 new_list.append(val)
             for value_list in val:
                 for value_print in value_list:
-                    list_newdata.append(value_print['value'])    
-        
+                    list_newdata.append(value_print['value'])
+
         for data in new_list:
             for list_data in data:
                 newlist_len =newlist_len + 1
-        
+
         remain_data = []
         counter = 0
         for newlist_data in list_newdata:
@@ -110,18 +110,18 @@ class report_dynamic_label(report_sxw.rml_parse):
                     remain_copy = number_of_copy - counter
                     for xx in range(0,remain_copy):
                         remain_data.append( newlist_data)
-        
+
         for data_value_vals in value_vals:
             if data_value_vals not in list_newdata:
                  for add_data in range(0,number_of_copy):
                      remain_data.append(data_value_vals)
-                 
+
         if newlist_len < number_of_copy:
             diff = number_of_copy - newlist_len
-        
+
         new_val = []
         list_newdata=[]
-        if len(ids) == 1:     
+        if len(ids) == 1:
             for new_result in range(0, diff):
                     result1.append(temp)
 
@@ -129,14 +129,14 @@ class report_dynamic_label(report_sxw.rml_parse):
                 for remain_data_value in remain_data:
                     temp[0]['value']=remain_data_value
                     result1.append([temp[0].copy()])
-                    
+
         for row in range(0, len(result1)):
                     new_val = result1[row*columns: row*columns + columns]
                     new_list.append(new_val)
         return new_list
-         
+
     def __init__(self, cr, uid, name, context):
-        
+
         super(report_dynamic_label,self).__init__(cr, uid, name, context=context)
         self.context=context
         self.rec_no = 0
